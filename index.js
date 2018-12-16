706049c7331f868fbbfee2bb31617a270423fb27
Monika Majer <majer_monika@interia.pl>
2018-12-16 13:16:54 +0100

    Change js code

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var fs = require('fs');
+var color = require('colors');
 
-fs.stat('./cat.jpg', function (err, stats) {
-  console.log(stats);
+fs.readdir('./', 'utf-8', function (err, files) {
+  console.log('Folder content:'.blue);
+  console.log('Output: ', files);
+  fs.writeFile('./new.txt', files, function (err) {
+    if (err) throw err;
+    console.log('Data saved!'.red);
+  });
 });
